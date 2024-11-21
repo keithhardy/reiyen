79fed9bb8d8942780ae8d24d2566a4cfa2f1cf4b
Keith Hardy <keithjnrhardy@gmail.com>
2024-11-21 23:10:37 +0000

    Update

--- lib/config.ts
+++ lib/config.ts
@@ -4,29 +4,29 @@ export const menuLinks = [
     href: '/dashboard',
     description: 'View an overview of system activities and key metrics in electrical certification.',
   },
-  {
-    title: 'Users',
-    href: '/dashboard/users',
-    description: 'Manage user accounts and permissions within the electrical certification system.',
-  },
-  {
-    title: 'Clients',
-    href: '/dashboard/clients',
-    description: 'Handle client information and scheduling for electrical services.',
-  },
-  {
-    title: 'Properties',
-    href: '/dashboard/properties',
-    description: 'Maintain records of properties requiring electrical certifications.',
-  },
-  {
-    title: 'Certificates',
-    href: '/dashboard/certificates',
-    description: 'Create, edit, and manage all types of electrical certificates.',
-  },
-  {
-    title: 'Settings',
-    href: '/dashboard/settings',
-    description: 'Configure system-wide settings for electrical certificate management.',
-  },
+  // {
+  //   title: 'Users',
+  //   href: '/dashboard/users',
+  //   description: 'Manage user accounts and permissions within the electrical certification system.',
+  // },
+  // {
+  //   title: 'Clients',
+  //   href: '/dashboard/clients',
+  //   description: 'Handle client information and scheduling for electrical services.',
+  // },
+  // {
+  //   title: 'Properties',
+  //   href: '/dashboard/properties',
+  //   description: 'Maintain records of properties requiring electrical certifications.',
+  // },
+  // {
+  //   title: 'Certificates',
+  //   href: '/dashboard/certificates',
+  //   description: 'Create, edit, and manage all types of electrical certificates.',
+  // },
+  // {
+  //   title: 'Settings',
+  //   href: '/dashboard/settings',
+  //   description: 'Configure system-wide settings for electrical certificate management.',
+  // },
 ];
