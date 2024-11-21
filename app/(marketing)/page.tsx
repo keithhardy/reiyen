1955936db2eb10547aad6aaae9207906323ac8fc
Keith Hardy <keithjnrhardy@gmail.com>
2024-11-21 23:07:54 +0000

    Update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
+import { auth0 } from "@/lib/auth0";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await auth0.getSession()
+  if (!session) redirect("/auth/login")
+
   return
 }
