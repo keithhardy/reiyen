0b47618a3aa470ea4d9f0a2d79264efb70042d23
Keith Hardy <keithjnrhardy@gmail.com>
2024-11-21 21:50:26 +0000

    Update

--- components/header/user-menu.tsx
+++ components/header/user-menu.tsx
@@ -22,7 +22,7 @@ export default function UserMenu({ user }: { user: any }) {
           <DropdownMenuLabel>{user.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
           <DropdownMenuItem asChild>
-            <Link href={`/users/${encodeURIComponent(user.sub)}/update`}>Settings</Link>
+            <Link href={`/users/${encodeURIComponent(user.sub)}/update`}>Profile</Link>
           </DropdownMenuItem>
             <DropdownMenuGroup>
               <DropdownMenuSub>
