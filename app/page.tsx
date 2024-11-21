f31e4ce6aa6fb34644a0669453dd581b246e83ba
Keith Hardy <keithjnrhardy@gmail.com>
2024-11-21 23:09:35 +0000

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { auth0 } from "@/lib/auth0";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth0.getSession()
-  if (!session) redirect("/auth/login")
-
-  return
+  redirect("/dashboard")
 }
