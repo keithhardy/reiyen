0a3a9161d4cd0d093a245ab9ba1bee89d371468f
Keith Hardy <keithjnrhardy@gmail.com>
2024-11-22 13:17:29 +0000

    Update

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -15,11 +15,11 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, label, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
             <div className="grid gap-1">
-              {label && <ToastTitle>{label}</ToastTitle>}
+              {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
                 <ToastDescription>{description}</ToastDescription>
               )}
