3578748f4362e9abcfc2a5baf80b4a896de6988b
Keith Hardy <keithjnrhardy@gmail.com>
2024-11-22 13:12:17 +0000

    Update

--- prisma/migrations/20241122131127_postgres/migration.sql
+++ prisma/migrations/20241122131127_postgres/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "Settings" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
@@ -8,88 +8,100 @@ CREATE TABLE "Settings" (
     "governingBody" TEXT NOT NULL,
     "governingBodyNumber" TEXT NOT NULL,
     "addressId" TEXT NOT NULL,
-    CONSTRAINT "Settings_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Settings_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Preferences" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "position" TEXT NOT NULL,
-    "signature" TEXT NOT NULL
+    "signature" TEXT NOT NULL,
+
+    CONSTRAINT "Preferences_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Qualification" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "awardingBody" TEXT NOT NULL,
     "qualification" TEXT NOT NULL,
     "qualificationNumber" TEXT NOT NULL,
     "awardDate" TEXT NOT NULL,
-    "certificateUrl" TEXT NOT NULL
+    "certificateUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Qualification_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Equipment" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "make" TEXT NOT NULL,
     "model" TEXT NOT NULL,
     "serialNumber" TEXT NOT NULL,
     "testDate" TEXT NOT NULL,
-    "certificateUrl" TEXT NOT NULL
+    "certificateUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Equipment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Permission" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "permission" TEXT NOT NULL,
     "clientId" TEXT,
-    CONSTRAINT "Permission_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+
+    CONSTRAINT "Permission_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Address" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "streetAddress" TEXT NOT NULL,
     "city" TEXT NOT NULL,
     "county" TEXT,
     "postTown" TEXT NOT NULL,
     "postcode" TEXT NOT NULL,
-    "country" TEXT NOT NULL DEFAULT 'United Kingdom'
+    "country" TEXT NOT NULL DEFAULT 'United Kingdom',
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Client" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "logoUrl" TEXT NOT NULL,
     "addressId" TEXT NOT NULL,
-    CONSTRAINT "Client_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Property" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "uprn" TEXT NOT NULL,
     "occupier" TEXT NOT NULL,
     "addressId" TEXT NOT NULL,
     "clientId" TEXT NOT NULL,
-    CONSTRAINT "Property_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "Property_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Property_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Certificate" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "propertyId" TEXT NOT NULL,
-    CONSTRAINT "Certificate_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "Property" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -115,3 +127,21 @@ CREATE INDEX "Property_clientId_idx" ON "Property"("clientId");
 
 -- CreateIndex
 CREATE INDEX "Certificate_propertyId_idx" ON "Certificate"("propertyId");
+
+-- AddForeignKey
+ALTER TABLE "Settings" ADD CONSTRAINT "Settings_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Permission" ADD CONSTRAINT "Permission_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Client" ADD CONSTRAINT "Client_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Property" ADD CONSTRAINT "Property_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "Address"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Property" ADD CONSTRAINT "Property_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Certificate" ADD CONSTRAINT "Certificate_propertyId_fkey" FOREIGN KEY ("propertyId") REFERENCES "Property"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
